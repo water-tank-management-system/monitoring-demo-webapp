5d1cf1c30e8bc221f5dc71b09f0ab9f44f05f86a
Naufal Muafi <nmuafi1@gmail.com>
2023-10-30 11:36:30 +0700

    add: ready to test

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,241 @@
+/* ========================================
+
+MONITORING WEB-APPS
+Firebase Project
+
+======================================== */
+
+
+// ===== Import Necessary Library/Packages =====
+
 #include <Arduino.h>
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
+#include <Firebase_ESP_Client.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Provide the token generation process info.
+#include "addons/TokenHelper.h"
+// Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+// ===== User-Defined Constant =====
+
+// Insert your network credentials
+#define WIFI_SSID "Muafi"
+#define WIFI_PASSWORD "pwnya11ya"
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyCCac6HO8_6z6UloSV_YNJwGE-iJjIlrQM"
+
+// Insert Authorized Email and Corresponding Password
+#define USER_EMAIL "nmuafi1@gmail.com"
+#define USER_PASSWORD "naufal153"
+
+// Insert RTDB URLefine the RTDB URL
+#define DATABASE_URL "https://monitoring-web-app-8d4fd-default-rtdb.asia-southeast1.firebasedatabase.app/"
+
+
+// ===== User-Defined Object =====
+
+// Define Firebase objects
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+
+// ===== User-Defined Variables =====
+
+// create counter
+int count = 0;
+
+int randNum = 0;
+
+// Data Parameters
+float temperature = 0;
+float water_level = 0;
+float water_flow = 0;
+int   turbidity = 0;
+bool  automation = false;
+
+// Variable to save USER UID
+String uid;
+
+// Variables to save database paths
+String databasePath;
+String tmpPath;
+String lvlPath;
+String flwPath;
+String turPath;
+String autPath;
+
+// Timer variables
+unsigned long sendDataPrevMillis = 0;
+unsigned long timerDelay = 60000; // send new readings every minute
+
+// ===== User-Defined Functions =====
+
+// Initialize WiFi Connection
+void initWiFi()
+{
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+  Serial.println();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// Write Float values to the database
+void sendFloat(String path, float value)
+{
+  if (Firebase.RTDB.setFloat(&fbdo, path.c_str(), value))
+  {
+    Serial.print("Writing value: ");
+    Serial.print(value);
+    Serial.print(" on the following path: ");
+    Serial.println(path);
+    Serial.println("PASSED");
+    Serial.println("PATH: " + fbdo.dataPath());
+    Serial.println("TYPE: " + fbdo.dataType());
+  }
+  else
+  {
+    Serial.println("FAILED");
+    Serial.println("REASON: " + fbdo.errorReason());
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// Write Int values to the database
+void sendInt(String path, int value)
+{
+  if (Firebase.RTDB.setInt(&fbdo, path.c_str(), value))
+  {
+    Serial.print("Writing value: ");
+    Serial.print(value);
+    Serial.print(" on the following path: ");
+    Serial.println(path);
+    Serial.println("PASSED");
+    Serial.println("PATH: " + fbdo.dataPath());
+    Serial.println("TYPE: " + fbdo.dataType());
+  }
+  else
+  {
+    Serial.println("FAILED");
+    Serial.println("REASON: " + fbdo.errorReason());
+  }
+}
+
+// Write Boolean values to the database
+void sendBool(String path, bool value)
+{
+  if (Firebase.RTDB.setBool(&fbdo, path.c_str(), value))
+  {
+    Serial.print("Writing value: ");
+    Serial.print(value);
+    Serial.print(" on the following path: ");
+    Serial.println(path);
+    Serial.println("PASSED");
+    Serial.println("PATH: " + fbdo.dataPath());
+    Serial.println("TYPE: " + fbdo.dataType());
+  }
+  else
+  {
+    Serial.println("FAILED");
+    Serial.println("REASON: " + fbdo.errorReason());
+  }
+}
+
+
+// ===== Let's Go! =====
+
+void setup()
+{
+  Serial.begin(115200);
+
+  randomSeed(analogRead(0));
+
+  // Initialize the WiFi Connection
+  initWiFi();
+
+  // Assign the api key (required)
+  config.api_key = API_KEY;
+
+  // Assign the user sign in credentials
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  // Assign the RTDB URL (required)
+  config.database_url = DATABASE_URL;
+
+  Firebase.reconnectWiFi(true);
+  fbdo.setResponseSize(4096);
+
+  // Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+  // Assign the maximum retry of token generation
+  config.max_token_generation_retry = 5;
+
+  // Initialize the library with the Firebase authen and config
+  Firebase.begin(&config, &auth);
+
+  // Getting the user UID might take a few seconds
+  Serial.println("Getting User UID");
+  while ((auth.token.uid) == "")
+  {
+    Serial.print('.');
+    delay(1000);
+  }
+  // Print user UID
+  uid = auth.token.uid.c_str();
+  Serial.print("User UID: ");
+  Serial.println(uid);
+
+  // Update database path
+  databasePath = "/UsersData/" + uid;
+
+  // Update database path for sensor readings
+  tmpPath = databasePath + "/temperature";  // --> UsersData/<user_uid>/temperature
+  turPath = databasePath + "/turbidity";    // --> UsersData/<user_uid>/turbidity
+  lvlPath = databasePath + "/water-level";  // --> UsersData/<user_uid>/water-level
+  flwPath = databasePath + "/water-flow";   // --> UsersData/<user_uid>/water-flow
+  autPath = databasePath + "/automation";   // --> UsersData/<user_uid>/automation
+}
+
+void loop()
+{    
+  // create random number
+  randNum = random(0, 100);
+
+  // Send new readings to database
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0))
+  {
+    sendDataPrevMillis = millis();
+
+    // Get parameters values
+    temperature = random(20, 40) + ((float)randNum / 100.0);
+    water_level = random(0, 100) + ((float)randNum / 100.0);
+    water_flow = random(0, 100) + ((float)randNum / 100.0);
+    turbidity = random(0, 50);
+    
+    if (count % 2 == 0) { automation = true; }
+    else { automation = false; }
+
+    // Send values to database:
+    sendFloat(tmpPath, temperature);
+    sendFloat(lvlPath, water_level);
+    sendFloat(flwPath, water_flow);
+    sendInt(turPath, turbidity);
+    sendBool(autPath, automation);
+  }
+
+  count++;
 }
