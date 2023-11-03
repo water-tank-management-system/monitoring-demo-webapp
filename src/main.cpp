9ea737219d9a3335291935bd44016c0a450a1279
Naufal Muafi <nmuafi1@gmail.com>
2023-11-03 10:03:27 +0700

    add: configuration for serial communication

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@ Firebase Project
 #endif
 #include <Firebase_ESP_Client.h>
 #include <Wire.h>
+#include <SoftwareSerial.h>
 
 // Provide the token generation process info.
 #include "addons/TokenHelper.h"
@@ -39,6 +40,10 @@ Firebase Project
 // Insert RTDB URLefine the RTDB URL
 #define DATABASE_URL "https://monitoring-web-app-8d4fd-default-rtdb.asia-southeast1.firebasedatabase.app/"
 
+// Serial Data Communication
+#define ESP_TX D1
+#define ESP_RX D2
+
 
 // ===== User-Defined Object =====
 
@@ -46,6 +51,9 @@ Firebase Project
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
+
+// Create Software Serial Object
+SoftwareSerial espSS(ESP_RX, ESP_TX);
 
 
 // ===== User-Defined Variables =====
@@ -73,6 +81,28 @@ String flwPath;
 String turPath;
 String autPath;
 
+// Serial Data Communication Variables
+bool  auto_mode = true,
+      cleaning_state_user = false,
+      fill_state_user = false;
+
+int state = ESP_RX;
+
+String  dataIn,
+        data1,
+        data2,
+        data3,
+        data4,
+        data5,
+        data6;
+
+uint8_t indexOfA,
+        indexOfB,
+        indexOfC,
+        indexOfD,
+        indexOfE,
+        indexOfF;
+
 // Timer variables
 unsigned long sendDataPrevMillis = 0;
 unsigned long timerDelay = 60000; // send new readings every minute
@@ -80,7 +110,89 @@ unsigned long timerDelay = 60000; // send new readings every minute
 // timer for LED
 unsigned long prev = 0;
 
-// ===== User-Defined Functions =====
+
+// ===== User-Defined Functions Declarations =====
+
+// Initialize WiFi Connection
+void initWiFi();
+
+// Initialize Firebase Configuration
+void initFirebase();
+
+// Write Float values to the database
+void sendFloat(String path, float value);
+
+// Write Int values to the database
+void sendInt(String path, int value);
+
+// Write Boolean values to the database
+void sendBool(String path, bool value);
+
+// Parse the Data from master
+void parsingData();
+
+// Create function to state the LED
+void stateLED(const long interval);
+
+
+// ===== Let's Go! =====
+
+void setup()
+{
+  // Baudrate for ESP-PC Communication
+  Serial.begin(9600);
+
+  // Baudrate for ESP - Arduino Communication
+  espSS.begin(9600);
+
+  // init Random seed for random number
+  randomSeed(analogRead(0));
+
+  // BUILTIN_LED initialize
+  pinMode(BUILTIN_LED, OUTPUT);
+  digitalWrite(BUILTIN_LED, LOW);
+
+  // Initialize the WiFi Connection
+  initWiFi();
+
+  // Initialize the Firebase 
+  initFirebase();
+}
+
+void loop()
+{    
+  // create random number
+  randNum = random(0, 100);
+
+  // Send new readings to database
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0))
+  {
+    sendDataPrevMillis = millis();
+
+    // Get parameters values
+    temperature = random(20, 40) + ((float)randNum / 100.0);
+    water_level = random(0, 100) + ((float)randNum / 100.0);
+    water_flow = random(0, 100) + ((float)randNum / 100.0);
+    turbidity = random(0, 50);
+    
+    if (count % 2 == 0) { automation = true; }
+    else { automation = false; }
+
+    // Send values to database:
+    sendFloat(tmpPath, temperature);
+    sendFloat(lvlPath, water_level);
+    sendFloat(flwPath, water_flow);
+    sendInt(turPath, turbidity);
+    sendBool(autPath, automation);
+
+    stateLED(2000);
+  }
+
+  count++;
+}
+
+
+// ===== Body of User-Defined Function =====
 
 // Initialize WiFi Connection
 void initWiFi()
@@ -94,6 +206,54 @@ void initWiFi()
   }
   Serial.println(WiFi.localIP());
   Serial.println();
+}
+
+// Initialize Firebase Configuration
+void initFirebase()
+{
+  // Assign the api key (required)
+  config.api_key = API_KEY;
+
+  // Assign the user sign in credentials
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  // Assign the RTDB URL (required)
+  config.database_url = DATABASE_URL;
+
+  Firebase.reconnectWiFi(true);
+  fbdo.setResponseSize(4096);
+
+  // Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+  // Assign the maximum retry of token generation
+  config.max_token_generation_retry = 5;
+
+  // Initialize the library with the Firebase authen and config
+  Firebase.begin(&config, &auth);
+
+  // Getting the user UID might take a few seconds
+  Serial.println("Getting User UID");
+  while ((auth.token.uid) == "")
+  {
+    Serial.print('.');
+    delay(1000);
+  }
+  // Print user UID
+  uid = auth.token.uid.c_str();
+  Serial.print("User UID: ");
+  Serial.println(uid);
+
+  // Update database path
+  databasePath = "/UsersData/" + uid;
+
+  // Update database path for sensor readings
+  tmpPath = databasePath + "/temperature"; // --> UsersData/<user_uid>/temperature
+  turPath = databasePath + "/turbidity";   // --> UsersData/<user_uid>/turbidity
+  lvlPath = databasePath + "/water-level"; // --> UsersData/<user_uid>/water-level
+  flwPath = databasePath + "/water-flow";  // --> UsersData/<user_uid>/water-flow
+  autPath = databasePath + "/automation";  // --> UsersData/<user_uid>/automation
 }
 
 // Write Float values to the database
@@ -156,6 +316,24 @@ void sendBool(String path, bool value)
   }
 }
 
+// Parse the Data from master
+void parsingData()
+{
+  indexOfA = dataIn.indexOf("A");
+  indexOfB = dataIn.indexOf("B");
+  indexOfC = dataIn.indexOf("C");
+  indexOfD = dataIn.indexOf("D");
+  indexOfE = dataIn.indexOf("E");
+  indexOfF = dataIn.indexOf("F");
+
+  data1 = dataIn.substring(0, indexOfA);
+  data2 = dataIn.substring(indexOfA + 1, indexOfB);
+  data3 = dataIn.substring(indexOfB + 1, indexOfC);
+  data4 = dataIn.substring(indexOfC + 1, indexOfD);
+  data5 = dataIn.substring(indexOfD + 1, indexOfE);
+  data6 = dataIn.substring(indexOfE + 1, indexOfF);
+}
+
 // Create function to state the LED
 void stateLED(const long interval)
 {
@@ -169,100 +347,5 @@ void stateLED(const long interval)
       digitalWrite(BUILTIN_LED, HIGH);
     else
       digitalWrite(BUILTIN_LED, LOW);
-  }  
-}
-
-
-// ===== Let's Go! =====
-
-void setup()
-{
-  // Baudrate for ESP-PC Communication
-  Serial.begin(115200);
-
-  // init Random seed for random number
-  randomSeed(analogRead(0));
-
-  // BUILTIN_LED initialize
-  pinMode(BUILTIN_LED, OUTPUT);
-  digitalWrite(BUILTIN_LED, LOW);
-
-  // Initialize the WiFi Connection
-  initWiFi();
-
-  // Assign the api key (required)
-  config.api_key = API_KEY;
-
-  // Assign the user sign in credentials
-  auth.user.email = USER_EMAIL;
-  auth.user.password = USER_PASSWORD;
-
-  // Assign the RTDB URL (required)
-  config.database_url = DATABASE_URL;
-
-  Firebase.reconnectWiFi(true);
-  fbdo.setResponseSize(4096);
-
-  // Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
-
-  // Assign the maximum retry of token generation
-  config.max_token_generation_retry = 5;
-
-  // Initialize the library with the Firebase authen and config
-  Firebase.begin(&config, &auth);
-
-  // Getting the user UID might take a few seconds
-  Serial.println("Getting User UID");
-  while ((auth.token.uid) == "")
-  {
-    Serial.print('.');
-    delay(1000);
   }
-  // Print user UID
-  uid = auth.token.uid.c_str();
-  Serial.print("User UID: ");
-  Serial.println(uid);
-
-  // Update database path
-  databasePath = "/UsersData/" + uid;
-
-  // Update database path for sensor readings
-  tmpPath = databasePath + "/temperature";  // --> UsersData/<user_uid>/temperature
-  turPath = databasePath + "/turbidity";    // --> UsersData/<user_uid>/turbidity
-  lvlPath = databasePath + "/water-level";  // --> UsersData/<user_uid>/water-level
-  flwPath = databasePath + "/water-flow";   // --> UsersData/<user_uid>/water-flow
-  autPath = databasePath + "/automation";   // --> UsersData/<user_uid>/automation
-}
-
-void loop()
-{    
-  // create random number
-  randNum = random(0, 100);
-
-  // Send new readings to database
-  if (Firebase.ready() && (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0))
-  {
-    sendDataPrevMillis = millis();
-
-    // Get parameters values
-    temperature = random(20, 40) + ((float)randNum / 100.0);
-    water_level = random(0, 100) + ((float)randNum / 100.0);
-    water_flow = random(0, 100) + ((float)randNum / 100.0);
-    turbidity = random(0, 50);
-    
-    if (count % 2 == 0) { automation = true; }
-    else { automation = false; }
-
-    // Send values to database:
-    sendFloat(tmpPath, temperature);
-    sendFloat(lvlPath, water_level);
-    sendFloat(flwPath, water_flow);
-    sendInt(turPath, turbidity);
-    sendBool(autPath, automation);
-
-    stateLED(2000);
-  }
-
-  count++;
 }
