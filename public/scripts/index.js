7d284aad1a664914ea98b4fed1dce18e59c92695
Naufal Muafi <nmuafi1@gmail.com>
2023-11-14 14:14:18 +0700

    add: index.js - table funciton

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -110,119 +110,8 @@ const setupUI = (user) => {
     var dbRefLog = firebase.database().ref(dbPathLog);
     var chartRef = firebase.database().ref(chartPath);
 
-    // CHECKBOXES
-    // Checbox (cards for sensor readings)
-    cardsCheckboxElement.addEventListener('change', (e) => {
-      if (cardsCheckboxElement.checked) {
-        cardsReadingsElement.style.display = 'block';
-      } else {
-        cardsReadingsElement.style.display = 'none';
-      }
-    });
-    // Checbox (gauges for sensor readings)
-    gaugesCheckboxElement.addEventListener('change', (e) => {
-      if (gaugesCheckboxElement.checked) {
-        gaugesReadingsElement.style.display = 'block';
-      } else {
-        gaugesReadingsElement.style.display = 'none';
-      }
-    });
-    // Checbox (charta for sensor readings)
-    chartsCheckboxElement.addEventListener('change', (e) => {
-      if (chartsCheckboxElement.checked) {
-        chartsDivElement.style.display = 'block';
-      } else {
-        chartsDivElement.style.display = 'none';
-      }
-    });
 
-    // CHARTS
-    // Number of Readings to plot on charts
-    var chartRange = 0;
-    // Get number of readings to plot saved on database (runs when the page first loads and whenever there's a change in the database)
-    chartRef.on('value', (snapshot) => {
-      chartRange = Number(snapshot.val());
-      console.log(chartRange);
-
-      // Delete all data from charts to update with new values when a new range is selected
-      chartT.destroy();
-      chartL.destroy();
-      chartF.destroy();
-      chartN.destroy();
-
-      // Render new charts to display new range of data
-      chartT = createTemperatureChart();
-      chartL = createWaterLevelChart();
-      chartF = createWaterFlowChart();
-      chartN = createTurbidityChart();
-
-      // Update the charts with the new range
-      // Get the latest readings and plot them on charts (the number of plotted readings corresponds to the chartRange value)
-      dbRefLog
-        .orderByKey()
-        .limitToLast(chartRange)
-        .on('child_added', (snapshot) => {
-          var jsonData = snapshot.toJSON(); // example: {temperature: 25.02, humidity: 50.20, pressure: 1008.48, timestamp:1641317355}
-          // Save values on variables
-          var temperature = jsonData.temperature;
-          var waterFlow = jsonData.water - flow;
-          var waterLevel = jsonData.water - level;
-          var turb = jsonData.turbidity;
-          var timestamp = jsonData.timestamp;
-
-          // Plot the values on the charts
-          plotValues(chartT, timestamp, temperature);
-          plotValues(chartL, timestamp, waterFlow);
-          plotValues(chartF, timestamp, waterLevel);
-          plotValues(chartN, timestamp, turb);
-        });
-    });
-
-    // Update database with new range (input field)
-    chartsRangeInputElement.onchange = () => {
-      chartRef.set(chartsRangeInputElement.value);
-    };
-
-    // GAUGES
-    dbRefLog
-      .orderByKey()
-      .limitToLast(1)
-      .on('child_added', (snapshot) => {
-        var jsonData = snapshot.toJSON(); // example: {temperature: 25.02, humidity: 50.20, pressure: 1008.48, timestamp:1641317355}
-        var temperature = jsonData.temperature;
-        var waterLevel = jsonData.water - level;
-        var turb = jsonData.turbidity;
-        var timestamp = jsonData.timestamp;
-
-        // Update DOM elements
-        var gaugeT = createTemperatureGauge();
-        var gaugeL = createWaterLevelGauge();
-        var gaugeN = createTurbidityGauge();
-
-        gaugeT.draw();
-        gaugeL.draw();
-        gaugeN.draw();
-
-        gaugeT.value = temperature;
-        gaugeL.value = waterLevel;
-        gaugeN.value = turb;
-
-        updateElement.innerHTML = epochToDateTime(timestamp);
-      });
-
-    // DELETE DATA
-    // Add event listener to open modal when click on "Delete Data" button
-    deleteButtonElement.addEventListener('click', (e) => {
-      console.log('Remove data');
-      e.preventDefault;
-      deleteModalElement.style.display = 'block';
-    });
-
-    // Add event listener when delete form is submited
-    deleteDataFormElement.addEventListener('submit', (e) => {
-      // delete data (readings)
-      dbRefLog.remove();
-    });
+    // CARDS
 
     // Switch on/off
     var autStatus;
@@ -401,8 +290,231 @@ const setupUI = (user) => {
       });
     });
 
-    // if user is logged out
-  } else {
+
+    // CHECKBOXES
+    // Checbox (cards for sensor readings)
+    cardsCheckboxElement.addEventListener('change', (e) => {
+      if (cardsCheckboxElement.checked) {
+        cardsReadingsElement.style.display = 'block';
+      } else {
+        cardsReadingsElement.style.display = 'none';
+      }
+    });
+    // Checbox (gauges for sensor readings)
+    gaugesCheckboxElement.addEventListener('change', (e) => {
+      if (gaugesCheckboxElement.checked) {
+        gaugesReadingsElement.style.display = 'block';
+      } else {
+        gaugesReadingsElement.style.display = 'none';
+      }
+    });
+    // Checbox (charta for sensor readings)
+    chartsCheckboxElement.addEventListener('change', (e) => {
+      if (chartsCheckboxElement.checked) {
+        chartsDivElement.style.display = 'block';
+      } else {
+        chartsDivElement.style.display = 'none';
+      }
+    });
+
+    // CHARTS
+    // Number of Readings to plot on charts
+    var chartRange = 0;
+    // Get number of readings to plot saved on database (runs when the page first loads and whenever there's a change in the database)
+    chartRef.on('value', (snapshot) => {
+      chartRange = Number(snapshot.val());
+      console.log(chartRange);
+
+      // Delete all data from charts to update with new values when a new range is selected
+      chartT.destroy();
+      chartL.destroy();
+      chartF.destroy();
+      chartN.destroy();
+
+      // Render new charts to display new range of data
+      chartT = createTemperatureChart();
+      chartL = createWaterLevelChart();
+      chartF = createWaterFlowChart();
+      chartN = createTurbidityChart();
+
+      // Update the charts with the new range
+      // Get the latest readings and plot them on charts (the number of plotted readings corresponds to the chartRange value)
+      dbRefLog
+        .orderByKey()
+        .limitToLast(chartRange)
+        .on('child_added', (snapshot) => {
+          var jsonData = snapshot.toJSON(); // example: {temperature: 25.02, humidity: 50.20, pressure: 1008.48, timestamp:1641317355}
+          // Save values on variables
+          var temperature = jsonData.temperature;
+          var waterFlow = jsonData.water-flow;
+          var waterLevel = jsonData.water-level;
+          var turb = jsonData.turbidity;
+          var timestamp = jsonData.timestamp;
+
+          // Plot the values on the charts
+          plotValues(chartT, timestamp, temperature);
+          plotValues(chartL, timestamp, waterFlow);
+          plotValues(chartF, timestamp, waterLevel);
+          plotValues(chartN, timestamp, turb);
+        });
+    });
+
+    // Update database with new range (input field)
+    chartsRangeInputElement.onchange = () => {
+      chartRef.set(chartsRangeInputElement.value);
+    };
+
+    // GAUGES
+    dbRefLog
+      .orderByKey()
+      .limitToLast(1)
+      .on('child_added', (snapshot) => {
+        var jsonData = snapshot.toJSON(); // example: {temperature: 25.02, humidity: 50.20, pressure: 1008.48, timestamp:1641317355}
+        var temperature = jsonData.temperature;
+        var waterLevel = jsonData.water - level;
+        var turb = jsonData.turbidity;
+        var timestamp = jsonData.timestamp;
+
+        // Update DOM elements
+        var gaugeT = createTemperatureGauge();
+        var gaugeL = createWaterLevelGauge();
+        var gaugeN = createTurbidityGauge();
+
+        gaugeT.draw();
+        gaugeL.draw();
+        gaugeN.draw();
+
+        gaugeT.value = temperature;
+        gaugeL.value = waterLevel;
+        gaugeN.value = turb;
+
+        updateElement.innerHTML = epochToDateTime(timestamp);
+      });
+
+    // DELETE DATA
+    // Add event listener to open modal when click on "Delete Data" button
+    deleteButtonElement.addEventListener('click', (e) => {
+      console.log('Remove data');
+      e.preventDefault;
+      deleteModalElement.style.display = 'block';
+    });
+
+    // Add event listener when delete form is submited
+    deleteDataFormElement.addEventListener('submit', (e) => {
+      // delete data (readings)
+      dbRefLog.remove();
+    });
+
+    // TABLE
+    var lastReadingTimestamp; //saves last timestamp displayed on the table
+
+    // Function that creates the table with the first 100 readings
+    function createTable() {
+      // append all data to the table
+      var firstRun = true;
+      dbRefLog
+        .orderByKey()
+        .limitToLast(100)
+        .on('child_added', function (snapshot) {
+          if (snapshot.exists()) {
+            var jsonData = snapshot.toJSON();
+            console.log(jsonData);
+
+            var temperature = jsonData.temperature;
+            var waterFlow = jsonData.water - flow;
+            var waterLevel = jsonData.water - level;
+            var turb = jsonData.turbidity;
+            var timestamp = jsonData.timestamp;
+            var content = '';
+
+            content += '<tr>';
+            content += '<td>' + epochToDateTime(timestamp) + '</td>';
+            content += '<td>' + temperature + '</td>';
+            content += '<td>' + waterFlow + '</td>';
+            content += '<td>' + waterLevel + '</td>';
+            content += '<td>' + turb + '</td>';
+            content += '</tr>';
+
+            $('#tbody').prepend(content);
+            // Save lastReadingTimestamp --> corresponds to the first timestamp on the returned snapshot data
+            if (firstRun) {
+              lastReadingTimestamp = timestamp;
+              firstRun = false;
+              console.log(lastReadingTimestamp);
+            }
+          }
+        });
+    }
+
+    // append readings to table (after pressing More results... button)
+    function appendToTable() {
+      var dataList = []; // saves list of readings returned by the snapshot (oldest-->newest)
+      var reversedList = []; // the same as previous, but reversed (newest--> oldest)
+      console.log('APEND');
+      dbRefLog
+        .orderByKey()
+        .limitToLast(100)
+        .endAt(lastReadingTimestamp)
+        .once('value', function (snapshot) {
+
+          // convert the snapshot to JSON
+          if (snapshot.exists()) {
+            snapshot.forEach((element) => {
+              var jsonData = element.toJSON();
+              dataList.push(jsonData); // create a list with all data
+            });
+            lastReadingTimestamp = dataList[0].timestamp; //oldest timestamp corresponds to the first on the list (oldest --> newest)
+            reversedList = dataList.reverse(); // reverse the order of the list (newest data --> oldest data)
+
+            var firstTime = true;
+            // loop through all elements of the list and append to table (newest elements first)
+            reversedList.forEach((element) => {
+              if (firstTime) {
+                // ignore first reading (it's already on the table from the previous query)
+                firstTime = false;
+              } else {
+                var temperature = element.temperature;
+                var waterFlow = element.waterFlow;
+                var waterLevel = element.waterLevel;
+                var turb = element.turb;
+                var timestamp = element.timestamp;
+                var content = '';
+
+                content += '<tr>';
+                content += '<td>' + epochToDateTime(timestamp) + '</td>';
+                content += '<td>' + temperature + '</td>';
+                content += '<td>' + waterFlow + '</td>';
+                content += '<td>' + waterLevel + '</td>';
+                content += '<td>' + turb + '</td>';
+                content += '</tr>';
+                $('#tbody').append(content);
+              }
+            });
+          }
+        });
+    }
+
+    viewDataButtonElement.addEventListener('click', (e) => {
+      // Toggle DOM elements
+      tableContainerElement.style.display = 'block';
+      viewDataButtonElement.style.display = 'none';
+      hideDataButtonElement.style.display = 'inline-block';
+      loadDataButtonElement.style.display = 'inline-block';
+      createTable();
+    });
+
+    loadDataButtonElement.addEventListener('click', (e) => {
+      appendToTable();
+    });
+
+    hideDataButtonElement.addEventListener('click', (e) => {
+      tableContainerElement.style.display = 'none';
+      viewDataButtonElement.style.display = 'inline-block';
+      hideDataButtonElement.style.display = 'none';
+    });
+    
+  }
+  else {  // if user is logged out
     // toggle UI elements
     loginElement.style.display = 'block';
     authBarElement.style.display = 'none';
